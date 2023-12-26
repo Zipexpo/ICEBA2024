ad83a174e6e28bc96e1076dadf3a3d49d58f15ed
Zipexpo <nvtngan@hcmus.edu.vn>
2023-12-26 10:53:39 +0700

    Update ImportantDate.js

--- src/components/sections/ImportantDate.js
+++ src/components/sections/ImportantDate.js
@@ -22,12 +22,12 @@ const defaultProps = {
 //     // {key:'Conference',date: ['12/6/2021','12/9/2021']},
 // ]
 const _event = [
-    {key:'Abstract submission and registration', date: '11/7/2024',href:submissionabstractLink},
-    {key:'Full paper submission for presentation',date: '11/15/2024', href:submissionfullLink},
-    {key:'Conference payment fee',date: '11/15/2024',prefix:'before', href:'./registration'},
-    {key:'Session schedule announcement',date: '11/20/2024', href:'./programme'},
-    {key:'Conference day',date: ['12/8/2024','12/9/2024'], href:'./programme'},
-    {key:'Full paper submission for publication',date: '12/30/2024', href:'./call-for-paper'},
+    {key:'Abstract submission and registration', date: '6/1/2024',href:submissionabstractLink},
+    {key:'Full paper submission for presentation',date: '6/7/2024', href:submissionfullLink},
+    {key:'Conference payment fee',date: '6/7/2024',prefix:'before', href:'./registration'},
+    {key:'Session schedule announcement',date: '6/20/2024', href:'./programme'},
+    {key:'Conference day',date: ['11/22/2024','11/23/2024'], href:'./programme'},
+    {key:'Full paper submission for publication',date: '11/30/2024', href:'./call-for-paper'},
     // {key:'Conference',date: ['12/6/2021','12/9/2021']},
 ]
 class ImportantDate extends React.Component {
