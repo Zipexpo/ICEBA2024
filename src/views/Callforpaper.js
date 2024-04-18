a016b515b7fc1db78191f53917b87d49e70f17f7
Zipexpo <nvtngan@hcmus.edu.vn>
2024-04-18 19:29:26 +0700

    Update Callforpaper.js

--- src/views/Callforpaper.js
+++ src/views/Callforpaper.js
@@ -185,7 +185,7 @@ class Callforpaper extends React.Component {
               >
                 ABSTRACT
               </Button>
-              <Button
+              {/* <Button
                 color="primary"
                 style={{ marginLeft: 5, marginRight: 5 }}
                 tag={"a"}
@@ -193,7 +193,7 @@ class Callforpaper extends React.Component {
                 href={submissionfullLink}
               >
                 FULL PAPER
-              </Button>
+              </Button> */}
             </p>
           </GenericSection>
 
